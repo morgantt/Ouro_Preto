0b8aac53efbaba3e11a63e5e8c83814d38772e33
Taithson Tmartins <taithson@gmail.com>
2011-07-22 17:17:40 -0300

    Initial comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OuroPreto::Application.config.secret_token = '0b31040a46b8998eefca53e5419c6027a54546739eb531fb11bcfd9bb9b8637e87416619d9b14c196ae17375b2fe58d5f034715f4bfa4ee663c14d35c1bc68c6'
