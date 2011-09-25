10b418ae39fb207041dba80e142df05b2d44a787
Taithson Tmartins <taithson@gmail.com>
2011-09-24 22:06:39 -0300

    Added a Sessions controler and a sign in view

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -25,6 +25,11 @@ it "should have a help page at '/help'" do
     response.should have_selector('title', :content => "Sign up")
   end
 
+  it "should have a Signin page at '/signin'" do
+    get '/signin'
+    response.should have_selector('title', :content => "Sign in")
+  end
+
   it "should have the right links on the layout" do
     visit root_path
     response.should have_selector('title', :content => "Home")
